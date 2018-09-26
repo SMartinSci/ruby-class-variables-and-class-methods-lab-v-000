00ef9829acb0e879520725fb54493c9f5efed6a1
Sherry Martin <sm674@cornell.edu>
2018-09-26 21:58:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,4 +1,4 @@
-class Song
+                                                                                                  class Song
   attr_accessor :name, :artists, :genre
   attr_reader :artist
   
